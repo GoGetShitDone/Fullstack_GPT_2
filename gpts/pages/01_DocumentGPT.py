9707af139080686cf4323440a19124911117dba1
Ullala David <rkdrms2020@icloud.com>
2024-08-30 21:21:01 +0900

    Update Project files

--- gpts/pages/01_DocumentGPT.py
+++ gpts/pages/01_DocumentGPT.py
@@ -1,17 +1,111 @@
+from langchain.prompts import ChatPromptTemplate
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
+from langchain.storage import LocalFileStore
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.callbacks.base import BaseCallbackHandler
 import streamlit as st
+
+st.set_page_config(
+	page_title="Document GPT",
+	page_icon="📄",
+)
+
+class ChatCallbackHandler(BaseCallbackHandler):
+    message = ""
+
+    def on_llm_start(self, *args, **kwargs):
+        self.message_box = st.empty()
+
+    def on_llm_end(self, *args, **kwargs):
+        save_message(self.message, "ai")
+
+    def on_llm_new_token(self, token, *args, **kwargs):
+        self.message += token
+        self.message_box.markdown(self.message)
+
+
+llm = ChatOpenAI(
+    temperature=0.1,
+    streaming=True,
+    callbacks=[
+        ChatCallbackHandler(),
+    ],
+)
+
+@st.cache_data(show_spinner="Embedding File...")
+def embed_file(file):
+	file_content = file.read()
+	file_path = f"./.cache/files/{file.name}"
+	with open(file_path, "wb") as f:
+		f.write(file_content)
+	cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+	splitter = CharacterTextSplitter.from_tiktoken_encoder(
+		separator="\n",
+		chunk_size=600,
+		chunk_overlap=100,
+	)
+	loader = UnstructuredFileLoader("./.cache/files/chapter_one.txt")
+	docs = loader.load_and_split(text_splitter=splitter)
+	embeddings = OpenAIEmbeddings()
+	cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+	vectorstore = FAISS.from_documents(docs, cached_embeddings)
+	retriever = vectorstore.as_retriever()
+	return retriever
+
+def save_message(message, role):
+	st.session_state["messages"].append({"message": message, "role": role})
+
+def send_message(message, role, save=True):
+	with st.chat_message(role):
+		st.markdown(message)
+	if save:
+		save_message(message, role)
+
+def paint_history():
+	for message in st.session_state["messages"]:
+		send_message(message["message"], message["role"], save=False,)
+
+def format_docs(docs):
+	return "\n\n".join(document.page_content for document in docs)
+
+prompt = ChatPromptTemplate.from_messages(
+	[
+		(
+            "system",
+            """
+            Answer the question using ONLY the following context. If you don't know the answer just say you don't know. DON'T make anything up.
+            
+            Context: {context}
+            """,
+        ),
+        ("human", "{question}"),
+	]
+)
 
 st.title("📄 Document GPT")
 
-st.markdown("""
-<div class="app-block">
-    <h2>Upload Your Document</h2>
-    <p>Upload a document and ask questions about its content.</p>
-</div>
-""", unsafe_allow_html=True)
+st.markdown("Welcome!~<br>Use this chatbot to ask questions to an AI about your files!!<br>Upload your files on the sideba!", unsafe_allow_html=True)
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    st.write("File uploaded successfully!")
-    st.text_input("Ask a question about your document:")
-    if st.button("Ask"):
-        st.write("This is where the answer would appear.")
+with st.sidebar:
+	file = st.file_uploader("Upload a .txt, .pdf, .docs file", type=["pdf","txt","docx",],)
+
+if file:
+	retriever = embed_file(file)
+	send_message("Good! Ask Anything!", "ai", save=False)
+	paint_history()
+	message = st.chat_input("Ask Anything! about your file...")
+	if message:
+		send_message(message, "human")
+		chain = ({
+			"context" : retriever | RunnableLambda(format_docs), 
+			"question": RunnablePassthrough(),
+			} | prompt | llm)		
+		with st.chat_message("ai"):
+			chain.invoke(message)
+			
+else:
+	st.session_state["messages"] = []
--- gpts/pages/03_QuizGPT.py
+++ gpts/pages/03_QuizGPT.py
@@ -1,3 +1,17 @@
 import streamlit as st
 
 st.title("🧐 Quiz GPT")
+
+st.markdown("""
+<div class="app-block">
+    <h2>Upload Your Document</h2>
+    <p>Upload a document and ask questions about its content.</p>
+</div>
+""", unsafe_allow_html=True)
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    st.write("File uploaded successfully!")
+    st.text_input("Ask a question about your document:")
+    if st.button("Ask"):
+        st.write("This is where the answer would appear.")

b8895f88b93b74114215b1d79f598a285f404304
Ullala David <rkdrms2020@icloud.com>
2024-09-01 20:20:40 +0900

    Using Memory(stuff), Manual chain

--- gpts/pages/03_QuizGPT.py
+++ gpts/pages/03_QuizGPT.py
@@ -1,6 +1,8 @@
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.callbacks import StreamingStdOutCallbackHandler
 import streamlit as st
 from langchain.retrievers import WikipediaRetriever
 
@@ -14,7 +16,9 @@ st.title("🧐 Quiz GPT")
 
 llm = ChatOpenAI(
     temperature=0.1,
-    mdide="gpt-3.5-turbo-1106"
+    model="gpt-3.5-turbo-1106",
+    streaming=True,
+    callbacks=[StreamingStdOutCallbackHandler()],
 )
 
 @st.cache_data(show_spinner="Loading file...")
@@ -32,6 +36,9 @@ def split_file(file):
     docs = loader.load_and_split(text_splitter=splitter)
     return docs
 
+def format_docs(docs):
+	return "\n\n".join(document.page_content for document in docs)
+
 with st.sidebar:
     docs = None
     choice = st.selectbox(
@@ -47,11 +54,45 @@ with st.sidebar:
     else:
         topic = st.text_input("Search Wikipidia...")
         if topic:
-            retriever = WikipediaRetriever(top_k_results=1)
+            retriever = WikipediaRetriever(top_k_results=1, lang="ko")
             with st.status("Searching Wikipedia..."):
                 docs = retriever.get_relevant_documents(topic)
 
 if not docs:
     st.markdown("Welcome to Quiz GPT.")
 else:
-    st.write(docs)
+
+    prompt = ChatPromptTemplate.from_messages([(
+    "system",
+    """
+    You are a helpful assistant that is role playing as a teacher.
+    
+    Based ONLY on the following context make 10 questions to test the user's knowledge about the text.
+    
+    Each question should have 4 answers, three of them must be incorrect and one should be correct.
+    
+    Use (o) to signal the correct answer.
+    
+    Question examples:
+    
+    Question: What is the color of the ocean?
+    Answers: Red|Yellow|Green|Blue(o)
+    
+    Question: What is the capital or Georgia?
+    Answers: Baku|Tbilisi(o)|Manila|Beirut
+    
+    Question: When was Avatar released?
+    Answers: 2007|2001|2009(o)|1998
+    
+    Question: Who was Julius Caesar?
+    Answers: A Roman Emperor(o)|Painter|Actor|Model
+    
+    Your turn!
+    Context: {context}""",)])
+
+    chain = {"context":format_docs} | prompt | llm
+
+    start = st.button("Generate Quiz")
+
+    if start:
+        chain.invoke(docs)
